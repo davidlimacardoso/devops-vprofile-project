86b25d9b28a4a4769832801ee921c81f74543388
davidlimacardoso <david.lima.cd@gmail.com>
2025-01-09 11:03:00 -0300

    feat: New eks IaC

--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
 variable "region" {
   description = "AWS region"
- type = string
- default = "us-east-1"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
- type = string
- default = "vpro-eks"
+  type        = string
+  default     = "vpro-labs-eks"
+}
+
+variable "vpc_id" {
+  default     = "vpc-028e5bd127ffb9444"
+  type        = string
+  description = "VPC ID to associate with EKS Cluster"
+}
+
+variable "private_subnets" {
+  default     = ["subnet-0c9478d9da82f0579", "subnet-04e972ab97a50a310"]
+  type        = list(string)
+  description = "Private Subnets for EKS Cluster"
 }
